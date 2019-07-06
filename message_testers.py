ea5ab4f2e88af9ed8ff2d23e88ece4ed9bc8ecbb
Akhil Narang <akhilnarang.1999@gmail.com>
2019-07-06 12:04:05 +0530

    jenkins: initial commit
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

---
+++ message_testers.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from sys import argv
+
+if len(argv) != 2:
+    print(f"Usage: {argv[0]} <codename>")
+    exit(1)
+
+testers = {
+    'berkeley': ['srisurya95'],
+    'beryllium': ['akhilnarang', 'grewal'],
+    'bullhead': ['anirudhgupta109', 'Skittles8923'],
+    'cheeseburger': ['HydroPetro', 'fryevia'],
+    'crosshatch': ['Fire69Flame'],
+    'dipper': ['argraur'],
+    'dumpling': ['divadsn', 'HydroPetro'],
+    'enchilada': ['anirudhgupta109'],
+    'fajita': ['nezorflame'],
+    'kiwi': ['coldhans'],
+    'mata': ['KuranKaname', 'Y45HW4N7'],
+    'mido': ['Adesh15'],
+    'oneplus3': ['HydroPetro', 'theshinybeast', 'nezorflame'],
+    'platina': ['nysadev'],
+    'potter': ['NickvBokhorst'],
+    'whyred': ['iwantz', 'raiadventures', 'ahmed_tohamy', 'Sanchith_Hegde', 'anunaym14_bot', 'bohrabhijeet', 'ZTR23'],
+    'x2': ['moto999999'],
+    'z2_plus': ['Pavan_Paps'],
+    'zl1': ['Gabronog']
+}
+
+device = argv[1]
+
+message = ""
+
+if device in testers.keys():
+    for tester in testers[device]:
+        message += f'@{tester} '
+    print(message)
+else:
+    print(f"Wrong device {device}(?)")
