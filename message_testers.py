617c3d2ff76825cbd405bfac2715c4a900d54165
Akhil Narang <akhilnarang.1999@gmail.com>
2020-01-19 19:46:15 +0530

    RIP Omkar
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- message_testers.py
+++ message_testers.py
@@ -10,7 +10,7 @@ testers = {
     "beryllium": ["akhilnarang", "grewal", "gr0ndpa", "Rokban", "LucentW", "ZTR23"],
     "cheeseburger": ["gotenksIN", "HydroPetro"],
     "dipper": ["argraur"],
-    "dumpling": ["HydroPetro", "edi194"]
+    "dumpling": ["HydroPetro", "edi194"],
     "enchilada": ["anirudhgupta109", "ronell1292k"],
     "fajita": ["ironhydee", "gautamhans1", "edi194"],
     "hotdogb": ["anirudhgupta109"],
