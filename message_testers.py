2fbafdad4c7e98097ec605a13400c26770715511
Akhil Narang <me@akhilnarang.dev>
2020-04-08 00:01:53 +0530

    jenkins: update testers, " -> ', add in some new repopicks and new jenkins build target lists
      * Also disable string normalization by default with black
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- message_testers.py
+++ message_testers.py
@@ -3,49 +3,52 @@
 from sys import argv
 
 if len(argv) != 2:
-    print("Usage: {} <codename>".format(argv[0]))
+    print('Usage: {} <codename>'.format(argv[0]))
     exit(1)
 
 testers = {
-    "beryllium": ["akhilnarang", "grewal", "gr0ndpa", "Rokban", "LucentW", "ZTR23"],
-    "bonito": ["Skittles9823"],
-    "cheeseburger": ["gotenksIN", "HydroPetro"],
-    "coral": ["XlxFoXxlX", "MrWilsonxD"],
-    "dipper": ["argraur"],
-    "dumpling": ["HydroPetro", "edi194"],
-    "enchilada": ["anirudhgupta109", "ronell1292k"],
-    "fajita": ["ironhydee", "gautamhans1", "edi194"],
-    "guacamole": ["anirudhgupta109", "RandomPooka", "nezorflame"],
-    "hotdogb": ["anirudhgupta109"],
-    "jasmine_sprout": ["Artsarino", "kenny3fcb"],
-    "mata": ["KuranKaname", "Y45HW4N7"],
-    "potter": ["NickvBokhorst", "Deadpool_61"],
-    "raphael": [
-        "ai94iq",
-        "BaaDNwZ",
-        "devanshbajaj",
-        "gr0ndpa",
-        "itsharhash",
-        "M86xKC",
-        "Pavan_Paps",
+    'beryllium': ['akhilnarang', 'grewal', 'gr0ndpa', 'Rokban', 'LucentW', 'ZTR23'],
+    'bonito': ['Skittles9823'],
+    'cheeseburger': ['gotenksIN', 'HydroPetro'],
+    'coral': ['XlxFoXxlX', 'MrWilsonxD'],
+    'dipper': ['argraur'],
+    'dumpling': ['HydroPetro', 'edi194'],
+    'enchilada': ['anirudhgupta109', 'ronell1292k'],
+    'fajita': ['ironhydee', 'gautamhans1', 'edi194'],
+    'guacamole': ['anirudhgupta109', 'RandomPooka', 'nezorflame'],
+    'guacamoleb': ['AshwinRC'],
+    'hotdogb': ['anirudhgupta109'],
+    'jasmine_sprout': ['Artsarino', 'kenny3fcb'],
+    'mata': ['KuranKaname', 'Y45HW4N7'],
+    'perseus': ['mjevange'],
+    'phoenix': ['akhilnarang'],
+    'potter': ['NickvBokhorst', 'Deadpool_61'],
+    'raphael': [
+        'ai94iq',
+        'BaaDNwZ',
+        'devanshbajaj',
+        'gr0ndpa',
+        'itsharhash',
+        'M86xKC',
+        'Pavan_Paps',
     ],
-    "sargo": ["Skittles9823"],
-    "taimen": ["Eamo5"],
-    "walleye": ["Eamo5", "repopic"],
-    "wayne": ["kenny3fcb"],
-    "whyred": ["iwantz", "ahmed_tohamy", "Sanchith_Hegde", "anunaym14", "Pavan_Paps"],
-    "x2": ["moto999999"],
-    "z2_plus": ["Pavan_Paps", "panicker666"],
-    "zl1": ["Gabronog", "LucentW"],
+    'sargo': ['Skittles9823'],
+    'taimen': ['Eamo5'],
+    'walleye': ['Eamo5', 'repopic'],
+    'wayne': ['kenny3fcb'],
+    'whyred': ['iwantz', 'ahmed_tohamy', 'Sanchith_Hegde', 'anunaym14', 'Pavan_Paps'],
+    'x2': ['moto999999'],
+    'z2_plus': ['Pavan_Paps', 'panicker666'],
+    'zl1': ['Gabronog', 'LucentW'],
 }
 
 device = argv[1]
 
-message = ""
+message = ''
 
 if device in testers.keys():
     for tester in testers[device]:
-        message += "@{} ".format(tester)
+        message += '@{} '.format(tester)
     print(message)
 else:
-    print("Wrong device {}(?)".format(device))
+    print('Wrong device {}(?)'.format(device))
